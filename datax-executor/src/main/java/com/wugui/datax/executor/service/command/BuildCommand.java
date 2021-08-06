ed24b62fb2e648a44f9d4fff95da87c5459fb541
tianzhiliang <tianzhiliang@cestc.com>
2021-08-06 09:58:00 +0800

    Initial commit

---
+++ datax-executor/src/main/java/com/wugui/datax/executor/service/command/BuildCommand.java
@@ -0,0 +1,172 @@
+package com.wugui.datax.executor.service.command;
+
+import com.wugui.datatx.core.biz.model.TriggerParam;
+import com.wugui.datatx.core.enums.IncrementTypeEnum;
+import com.wugui.datatx.core.log.JobLogger;
+import com.wugui.datatx.core.util.Constants;
+import com.wugui.datatx.core.util.DateUtil;
+import com.wugui.datax.executor.util.SystemUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static com.wugui.datatx.core.util.Constants.SPLIT_COMMA;
+import static com.wugui.datax.executor.service.jobhandler.DataXConstant.*;
+
+/**
+ * DataX command build
+ *
+ * @author jingwk 2020-06-07
+ */
+public class BuildCommand {
+
+    /**
+     * DataX command build
+     *
+     * @param tgParam
+     * @param tmpFilePath
+     * @param dataXPyPath
+     * @return
+     */
+    public static String[] buildDataXExecutorCmd(TriggerParam tgParam, String tmpFilePath, String dataXPyPath) {
+        // command process
+        //"--loglevel=debug"
+        List<String> cmdArr = new ArrayList<>();
+        cmdArr.add("python");
+        String dataXHomePath = SystemUtils.getDataXHomePath();
+        if (StringUtils.isNotEmpty(dataXHomePath)) {
+            dataXPyPath = dataXHomePath.contains("bin") ? dataXHomePath + DEFAULT_DATAX_PY : dataXHomePath + "bin" + File.separator + DEFAULT_DATAX_PY;
+        }
+        cmdArr.add(dataXPyPath);
+        String doc = buildDataXParam(tgParam);
+        if (StringUtils.isNotBlank(doc)) {
+            cmdArr.add(doc.replaceAll(SPLIT_SPACE, TRANSFORM_SPLIT_SPACE));
+        }
+        cmdArr.add(tmpFilePath);
+        return cmdArr.toArray(new String[cmdArr.size()]);
+    }
+
+    /**
+     * 构建datax运行虚拟机参数
+     *
+     * @param tgParam
+     * @return {@link String}
+     * @author Locki
+     * @date 2020/9/18
+     */
+    private static String buildDataXParam(TriggerParam tgParam) {
+        StringBuilder doc = new StringBuilder();
+        String jvmParam = StringUtils.isNotBlank(tgParam.getJvmParam()) ? tgParam.getJvmParam().trim() : tgParam.getJvmParam();
+        if (StringUtils.isNotBlank(jvmParam)) {
+            doc.append(JVM_CM).append(TRANSFORM_QUOTES).append(jvmParam).append(TRANSFORM_QUOTES);
+        }
+        return doc.toString();
+    }
+
+    /**
+     * 构建datax增量参数
+     *
+     * @param tgParam
+     * @return {@link HashMap< String, String>}
+     * @author Locki
+     * @date 2020/9/18
+     */
+    public static HashMap<String, String> buildDataXParamToMap(TriggerParam tgParam) {
+        String partitionStr = tgParam.getPartitionInfo();
+        Integer incrementType = tgParam.getIncrementType();
+        String replaceParam = StringUtils.isNotBlank(tgParam.getReplaceParam()) ? tgParam.getReplaceParam().trim() : null;
+        if (incrementType != null && replaceParam != null) {
+            if (IncrementTypeEnum.ID.getCode() == incrementType) {
+                long startId = tgParam.getStartId();
+                long endId = tgParam.getEndId();
+                String formatParam = String.format(replaceParam, startId, endId);
+                return getKeyValue(formatParam);
+            } else if (IncrementTypeEnum.TIME.getCode() == incrementType) {
+                String replaceParamType = tgParam.getReplaceParamType();
+                if (StringUtils.isBlank(replaceParamType) || "Timestamp".equals(replaceParamType)) {
+                    long startTime = tgParam.getStartTime().getTime() / 1000;
+                    long endTime = tgParam.getTriggerTime().getTime() / 1000;
+                    String formatParam = String.format(replaceParam, startTime, endTime);
+                    return getKeyValue(formatParam);
+                } else {
+                    SimpleDateFormat sdf = new SimpleDateFormat(replaceParamType);
+                    String endTime = sdf.format(tgParam.getTriggerTime());
+                    String startTime = sdf.format(tgParam.getStartTime());
+
+                    String formatParam = String.format(replaceParam, startTime, endTime);
+                    return getKeyValue(formatParam);
+                }
+            }
+        }
+
+        if (incrementType != null && IncrementTypeEnum.PARTITION.getCode() == incrementType) {
+            if (StringUtils.isNotBlank(partitionStr)) {
+                List<String> partitionInfo = Arrays.asList(partitionStr.split(SPLIT_COMMA));
+                String formatParam = String.format(PARAMS_CM_V_PT, buildPartition(partitionInfo));
+                return getKeyValue(formatParam);
+            }
+
+        }
+        return null;
+    }
+
+    /**
+     * 任务参数封装为map
+     *
+     * @param formatParam
+     * @return {@link HashMap< String, String>}
+     * @author Locki
+     * @date 2020/9/18
+     */
+    private static HashMap<String, String> getKeyValue(String formatParam) {
+        String[] paramArr = formatParam.split(PARAMS_SYSTEM);
+        HashMap<String, String> map = new HashMap<String, String>();
+
+        for (String param : paramArr) {
+            if (StringUtils.isNotBlank(param)) {
+                param = param.trim();
+                String[] keyValue = param.split(PARAMS_EQUALS);
+                map.put(keyValue[0], keyValue[1]);
+            }
+        }
+        return map;
+    }
+
+    /**
+     * datax任务内置变量：模仿阿里云商用DataWorks/ODPS提供内置变量<br/>
+     * ${datax_bizdate}
+     * ${datax_biztime}
+     * ${datax_biz_unixtimestamp}
+     *
+     * @param
+     * @return {@link Map< String, String>}
+     * @author Locki
+     * @date 2020/9/18
+     */
+    public static Map<String, String> builtInVar(){
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("datax_biz_date", DateUtil.format(new Date(), "yyyy-MM-dd"));
+        map.put("datax_biz_time", DateUtil.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+        map.put("datax_biz_unixtimestamp", System.currentTimeMillis() + "");
+        return map;
+    }
+
+    private void buildPartitionCM(StringBuilder doc, String partitionStr) {
+        if (StringUtils.isNotBlank(partitionStr)) {
+            doc.append(SPLIT_SPACE);
+            List<String> partitionInfo = Arrays.asList(partitionStr.split(SPLIT_COMMA));
+            doc.append(String.format(PARAMS_CM_V_PT, buildPartition(partitionInfo)));
+        }
+    }
+
+    private static String buildPartition(List<String> partitionInfo) {
+        String field = partitionInfo.get(0);
+        int timeOffset = Integer.parseInt(partitionInfo.get(1));
+        String timeFormat = partitionInfo.get(2);
+        String partitionTime = DateUtil.format(DateUtil.addDays(new Date(), timeOffset), timeFormat);
+        return field + Constants.EQUAL + partitionTime;
+    }
+
+}
---
+++ datax-executor/src/main/java/com/wugui/datax/executor/service/jobhandler/ExecutorJobHandler.java
@@ -0,0 +1,162 @@
+package com.wugui.datax.executor.service.jobhandler;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
+import com.wugui.datatx.core.biz.model.HandleProcessCallbackParam;
+import com.wugui.datatx.core.biz.model.ReturnT;
+import com.wugui.datatx.core.biz.model.TriggerParam;
+import com.wugui.datatx.core.handler.IJobHandler;
+import com.wugui.datatx.core.handler.annotation.JobHandler;
+import com.wugui.datatx.core.log.JobLogger;
+import com.wugui.datatx.core.thread.ProcessCallbackThread;
+import com.wugui.datatx.core.util.ProcessUtil;
+import com.wugui.datax.executor.service.logparse.LogStatistics;
+import com.wugui.datax.executor.util.SystemUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.FutureTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.wugui.datax.executor.service.command.BuildCommand.*;
+import static com.wugui.datax.executor.service.jobhandler.DataXConstant.DEFAULT_JSON;
+import static com.wugui.datax.executor.service.logparse.AnalysisStatistics.analysisStatisticsLog;
+
+/**
+ * DataX任务运行
+ *
+ * @author jingwk 2019-11-16
+ */
+
+@JobHandler(value = "executorJobHandler")
+@Component
+public class ExecutorJobHandler extends IJobHandler {
+
+    @Value("${datax.executor.jsonpath}")
+    private String jsonPath;
+
+    @Value("${datax.pypath}")
+    private String dataXPyPath;
+
+    private  static final Pattern VARIABLE_PATTERN = Pattern.compile("(\\$)\\{?(\\w+)\\}?");
+
+    @Override
+    public ReturnT<String> execute(TriggerParam trigger) {
+
+        int exitValue = -1;
+        Thread errThread = null;
+        String tmpFilePath;
+        LogStatistics logStatistics = null;
+
+        HashMap<String, String> keyValueMap = buildDataXParamToMap(trigger);
+        String jobJson = replaceVariable(trigger.getJobJson(), keyValueMap);
+        Map<String, String> buildin = builtInVar();
+        jobJson = replaceVariable(jobJson, buildin);
+
+        //Generate JSON temporary file
+        tmpFilePath = generateTemJsonFile(jobJson);
+
+        try {
+            String[] cmdarrayFinal = buildDataXExecutorCmd(trigger, tmpFilePath,dataXPyPath);
+            final Process process = Runtime.getRuntime().exec(cmdarrayFinal);
+            String prcsId = ProcessUtil.getProcessId(process);
+            JobLogger.log("------------------DataX process id: " + prcsId);
+            jobTmpFiles.put(prcsId, tmpFilePath);
+            //update datax process id
+            HandleProcessCallbackParam prcs = new HandleProcessCallbackParam(trigger.getLogId(), trigger.getLogDateTime(), prcsId);
+            ProcessCallbackThread.pushCallBack(prcs);
+            // log-thread
+            Thread futureThread = null;
+            FutureTask<LogStatistics> futureTask = new FutureTask<>(() -> analysisStatisticsLog(new BufferedInputStream(process.getInputStream())));
+            futureThread = new Thread(futureTask);
+            futureThread.start();
+
+            errThread = new Thread(() -> {
+                try {
+                    analysisStatisticsLog(new BufferedInputStream(process.getErrorStream()));
+                } catch (IOException e) {
+                    JobLogger.log(e);
+                }
+            });
+
+            logStatistics = futureTask.get();
+            errThread.start();
+            // process-wait
+            exitValue = process.waitFor();      // exit code: 0=success, 1=error
+            // log-thread join
+            errThread.join();
+        } catch (Exception e) {
+            JobLogger.log(e);
+        } finally {
+            if (errThread != null && errThread.isAlive()) {
+                errThread.interrupt();
+            }
+            //  删除临时文件
+            if (FileUtil.exist(tmpFilePath)) {
+                FileUtil.del(new File(tmpFilePath));
+            }
+        }
+        if (exitValue == 0) {
+            return new ReturnT<>(200, logStatistics.toString());
+        } else {
+            return new ReturnT<>(IJobHandler.FAIL.getCode(), "command exit value(" + exitValue + ") is failed");
+        }
+    }
+
+    /**
+     * 替换json变量
+     *
+     * @param param
+     * @param variableMap
+     * @return {@link String}
+     * @author Locki
+     * @date 2020/9/18
+     */
+    public static String replaceVariable(final String param, Map<String, String> variableMap) {
+        if (variableMap == null || variableMap.size() < 1) {
+            return param;
+        }
+        Map<String, String> mapping = new HashMap<String, String>();
+
+        Matcher matcher = VARIABLE_PATTERN.matcher(param);
+        while (matcher.find()) {
+            String variable = matcher.group(2);
+            String value = variableMap.get(variable);
+            if (StringUtils.isBlank(value)) {
+                value = matcher.group();
+            }
+            mapping.put(matcher.group(), value);
+        }
+
+        String retString = param;
+        for (final String key : mapping.keySet()) {
+            retString = retString.replace(key, mapping.get(key));
+        }
+        return retString;
+    }
+
+    private String generateTemJsonFile(String jobJson) {
+        String tmpFilePath;
+        String dataXHomePath = SystemUtils.getDataXHomePath();
+        if (StringUtils.isNotEmpty(dataXHomePath)) {
+            jsonPath = dataXHomePath + DEFAULT_JSON;
+        }
+        if (!FileUtil.exist(jsonPath)) {
+            FileUtil.mkdir(jsonPath);
+        }
+        tmpFilePath = jsonPath + "jobTmp-" + IdUtil.simpleUUID() + ".conf";
+        // 根据json写入到临时本地文件
+        try (PrintWriter writer = new PrintWriter(tmpFilePath, "UTF-8")) {
+            writer.println(jobJson);
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            JobLogger.log("JSON 临时文件写入异常：" + e.getMessage());
+        }
+        return tmpFilePath;
+    }
+
+}
